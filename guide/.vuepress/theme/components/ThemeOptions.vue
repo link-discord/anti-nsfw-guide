693749f68ec79c19195aa0270e4236418fba71ab
Link <50463727+link-discord@users.noreply.github.com>
2021-08-14 13:32:59 +0200

    format vue files

--- guide/.vuepress/theme/components/ThemeOptions.vue
+++ guide/.vuepress/theme/components/ThemeOptions.vue
@@ -1,100 +1,96 @@
 <template>
-	<div class="theme-options">
-		<ul class="color-theme-options">
-			<span>Theme</span>
-			<li>
-				<button
-					class="default-theme"
-					title="Green theme"
-					@click.prevent="setTheme({ persist: true })"
-				></button>
-			</li>
-			<li>
-				<button
-					class="blurple-theme"
-					title="Blurple theme"
-					@click.prevent="setTheme({ colorTheme: 'blurple', persist: true })"
-				></button>
-			</li>
-			<li>
-				<button
-					class="purple-theme"
-					title="Purple theme"
-					@click.prevent="setTheme({ colorTheme: 'purple', persist: true })"
-				></button>
-			</li>
+    <div class="theme-options">
+        <ul class="color-theme-options">
+            <span>Theme</span>
+            <li>
+                <button class="default-theme" title="Green theme" @click.prevent="setTheme({ persist: true })"></button>
+            </li>
+            <li>
+                <button
+                    class="blurple-theme"
+                    title="Blurple theme"
+                    @click.prevent="setTheme({ colorTheme: 'blurple', persist: true })"
+                ></button>
+            </li>
+            <li>
+                <button
+                    class="purple-theme"
+                    title="Purple theme"
+                    @click.prevent="setTheme({ colorTheme: 'purple', persist: true })"
+                ></button>
+            </li>
 
-			<li>
-				<button
-					class="orange-theme"
-					title="Orange theme"
-					@click.prevent="setTheme({ colorTheme: 'orange', persist: true })"
-				></button>
-			</li>
-		</ul>
-	</div>
+            <li>
+                <button
+                    class="orange-theme"
+                    title="Orange theme"
+                    @click.prevent="setTheme({ colorTheme: 'orange', persist: true })"
+                ></button>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup lang="ts">
-import useColorTheme from '../composables/useColorTheme';
+import useColorTheme from '../composables/useColorTheme'
 
-const { setTheme } = useColorTheme();
+const { setTheme } = useColorTheme()
 </script>
 
 <style lang="scss">
 .theme-options {
-	.color-theme-options {
-		display: flex;
+    .color-theme-options {
+        display: flex;
 
-		> span {
-			margin-right: auto;
-		}
+        > span {
+            margin-right: auto;
+        }
 
-		li {
-			margin-left: 0.5rem;
+        li {
+            margin-left: 0.5rem;
 
-			button {
-				color: #fff;
-				display: block;
-				width: 1.25rem;
-				height: 1.25rem;
-				border: 0;
-				border-radius: 2px;
-				cursor: pointer;
+            button {
+                color: #fff;
+                display: block;
+                width: 1.25rem;
+                height: 1.25rem;
+                border: 0;
+                border-radius: 2px;
+                cursor: pointer;
 
-				&.default-theme {
-					background-color: var(--green-500);
+                &.default-theme {
+                    background-color: var(--green-500);
 
-					&:hover {
-						background-color: var(--green-530)
-					}
-				}
+                    &:hover {
+                        background-color: var(--green-530);
+                    }
+                }
 
-				&.blurple-theme {
-					background-color: var(--blurple-500);
+                &.blurple-theme {
+                    background-color: var(--blurple-500);
 
-					&:hover {
-						background-color: var(--blurple-530)
-					}
-				}
+                    &:hover {
+                        background-color: var(--blurple-530);
+                    }
+                }
 
-				&.purple-theme {
-					background-color: var(--purple-500);
+                &.purple-theme {
+                    background-color: var(--purple-500);
 
-					&:hover {
-						background-color: var(--purple-530)
-					}
-				}
+                    &:hover {
+                        background-color: var(--purple-530);
+                    }
+                }
 
-				&.orange-theme {
-					background-color: var(--orange-500);
+                &.orange-theme {
+                    background-color: var(--orange-500);
 
-					&:hover {
-						background-color: var(--orange-530)
-					}
-				}
-			}
-		}
-	}
+                    &:hover {
+                        background-color: var(--orange-530);
+                    }
+                }
+            }
+        }
+    }
 }
 </style>
--- guide/.vuepress/theme/layouts/Layout.vue
+++ guide/.vuepress/theme/layouts/Layout.vue
@@ -1,124 +1,118 @@
 <template>
-	<div
-		class="theme-container"
-		:class="containerClass"
-		@touchstart="onTouchStart"
-		@touchend="onTouchEnd"
-	>
-		<Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar">
-			<template #before>
-				<slot name="navbar-before"></slot>
-			</template>
-			<template #after>
-				<slot name="navbar-after"></slot>
-			</template>
-		</Navbar>
-		<div class="content-wrapper">
-			<Notifications>
-			</Notifications>
-			<div class="sidebar-mask" @click="toggleSidebar(false)"></div>
-			<div class="sidebar-wrapper">
-				<Sidebar>
-					<template #top>
-						<slot name="sidebar-top"></slot>
-					</template>
-					<template #bottom>
-						<slot name="sidebar-bottom"></slot>
-					</template>
-				</Sidebar>
-			</div>
-			<Home v-if="frontmatter.home" />
-			<Transition
-				v-else
-				name="fade-slide-y"
-				mode="out-in"
-				@before-enter="onBeforeEnter"
-				@before-leave="onBeforeLeave"
-			>
-				<Page :key="page.path">
-					<template #top>
-						<slot name="page-top"></slot>
-					</template>
-					<template #bottom>
-						<slot name="page-bottom"></slot>
-					</template>
-				</Page>
-			</Transition>
-		</div>
-	</div>
+    <div class="theme-container" :class="containerClass" @touchstart="onTouchStart" @touchend="onTouchEnd">
+        <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar">
+            <template #before>
+                <slot name="navbar-before"></slot>
+            </template>
+            <template #after>
+                <slot name="navbar-after"></slot>
+            </template>
+        </Navbar>
+        <div class="content-wrapper">
+            <Notifications> </Notifications>
+            <div class="sidebar-mask" @click="toggleSidebar(false)"></div>
+            <div class="sidebar-wrapper">
+                <Sidebar>
+                    <template #top>
+                        <slot name="sidebar-top"></slot>
+                    </template>
+                    <template #bottom>
+                        <slot name="sidebar-bottom"></slot>
+                    </template>
+                </Sidebar>
+            </div>
+            <Home v-if="frontmatter.home" />
+            <Transition
+                v-else
+                name="fade-slide-y"
+                mode="out-in"
+                @before-enter="onBeforeEnter"
+                @before-leave="onBeforeLeave"
+            >
+                <Page :key="page.path">
+                    <template #top>
+                        <slot name="page-top"></slot>
+                    </template>
+                    <template #bottom>
+                        <slot name="page-bottom"></slot>
+                    </template>
+                </Page>
+            </Transition>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, onUnmounted, ref, Transition } from 'vue';
-import { useRouter } from 'vue-router';
-import { usePageData, usePageFrontmatter } from '@vuepress/client';
-import type { DefaultThemePageFrontmatter } from '@vuepress/theme-default/lib/shared';
-import Home from '@vuepress/theme-default/lib/client/components/Home.vue';
-import Page from '@vuepress/theme-default/lib/client/components/Page.vue';
-import { useScrollPromise, useSidebarItems, useThemeLocaleData } from '@vuepress/theme-default/lib/client/composables';
-import Navbar from '../components/Navbar.vue';
-import Sidebar from '../components/Sidebar.vue';
-import Notifications from '../components/Notifications.vue';
-import Notification from '../components/Notification.vue';
-import PartyPopperIcon from '../components/icons/PartyPopper.vue';
+import { computed, onMounted, onUnmounted, ref, Transition } from 'vue'
+import { useRouter } from 'vue-router'
+import { usePageData, usePageFrontmatter } from '@vuepress/client'
+import type { DefaultThemePageFrontmatter } from '@vuepress/theme-default/lib/shared'
+import Home from '@vuepress/theme-default/lib/client/components/Home.vue'
+import Page from '@vuepress/theme-default/lib/client/components/Page.vue'
+import { useScrollPromise, useSidebarItems, useThemeLocaleData } from '@vuepress/theme-default/lib/client/composables'
+import Navbar from '../components/Navbar.vue'
+import Sidebar from '../components/Sidebar.vue'
+import Notifications from '../components/Notifications.vue'
+import Notification from '../components/Notification.vue'
+import PartyPopperIcon from '../components/icons/PartyPopper.vue'
 
-const page = usePageData();
-const frontmatter = usePageFrontmatter<DefaultThemePageFrontmatter>();
-const themeLocale = useThemeLocaleData();
+const page = usePageData()
+const frontmatter = usePageFrontmatter<DefaultThemePageFrontmatter>()
+const themeLocale = useThemeLocaleData()
 
 // navbar
 const shouldShowNavbar = computed(() => {
-	return frontmatter.value.navbar !== false && themeLocale.value.navbar !== false;
-});
+    return frontmatter.value.navbar !== false && themeLocale.value.navbar !== false
+})
 
 // sidebar
-const sidebarItems = useSidebarItems();
-const isSidebarOpen = ref(false);
+const sidebarItems = useSidebarItems()
+const isSidebarOpen = ref(false)
 const toggleSidebar = (to?: boolean): void => {
-	isSidebarOpen.value = typeof to === 'boolean' ? to : !isSidebarOpen.value;
-};
+    isSidebarOpen.value = typeof to === 'boolean' ? to : !isSidebarOpen.value
+}
 
-const touchStart = { x: 0, y: 0 };
+const touchStart = { x: 0, y: 0 }
 const onTouchStart = (event): void => {
-	touchStart.x = event.changedTouches[0].clientX;
-	touchStart.y = event.changedTouches[0].clientY;
-};
+    touchStart.x = event.changedTouches[0].clientX
+    touchStart.y = event.changedTouches[0].clientY
+}
 
 const onTouchEnd = (event): void => {
-	const dx = event.changedTouches[0].clientX - touchStart.x;
-	const dy = event.changedTouches[0].clientY - touchStart.y;
+    const dx = event.changedTouches[0].clientX - touchStart.x
+    const dy = event.changedTouches[0].clientY - touchStart.y
 
-	if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-		toggleSidebar(dx > 0 && touchStart.x <= 80);
-	}
-};
+    if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+        toggleSidebar(dx > 0 && touchStart.x <= 80)
+    }
+}
 
 // classes
 const containerClass = computed(() => [
-	{
-		'no-navbar': !shouldShowNavbar.value,
-		'no-sidebar': !sidebarItems.value.length,
-		'sidebar-open': isSidebarOpen.value,
-	},
-	frontmatter.value.pageClass,
-]);
+    {
+        'no-navbar': !shouldShowNavbar.value,
+        'no-sidebar': !sidebarItems.value.length,
+        'sidebar-open': isSidebarOpen.value
+    },
+    frontmatter.value.pageClass
+])
 
 // close sidebar after navigation
-let unregisterRouterHook;
+let unregisterRouterHook
 
 onMounted(() => {
-	const router = useRouter();
-	unregisterRouterHook = router.afterEach(() => {
-		toggleSidebar(false);
-	});
-});
+    const router = useRouter()
+    unregisterRouterHook = router.afterEach(() => {
+        toggleSidebar(false)
+    })
+})
 
 onUnmounted(() => {
-	unregisterRouterHook();
-});
+    unregisterRouterHook()
+})
 
 // handle scrollBehavior with transition
-const scrollPromise = useScrollPromise();
-const onBeforeEnter = scrollPromise.resolve;
-const onBeforeLeave = scrollPromise.pending;
+const scrollPromise = useScrollPromise()
+const onBeforeEnter = scrollPromise.resolve
+const onBeforeLeave = scrollPromise.pending
 </script>
