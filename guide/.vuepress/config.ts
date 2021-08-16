7319064c3d5e63cf32bcf21e5772410d86b0771b
Link <50463727+link-discord@users.noreply.github.com>
2021-08-16 10:27:36 +0200

    update: change theme color

--- guide/.vuepress/config.ts
+++ guide/.vuepress/config.ts
@@ -16,7 +16,7 @@ const config = defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
         ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
         ['link', { rel: 'icon', href: '/favicon.png' }],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
-        ['meta', { name: 'theme-color', content: '#fb0881' }],
+        ['meta', { name: 'theme-color', content: '#1d1d1d' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
         ['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
