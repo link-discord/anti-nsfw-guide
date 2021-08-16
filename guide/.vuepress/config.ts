4a98d17d57952542e59c87eed72dba11d88b2fab
Link <50463727+link-discord@users.noreply.github.com>
2021-08-16 10:46:25 +0200

    Update config.ts

--- guide/.vuepress/config.ts
+++ guide/.vuepress/config.ts
@@ -29,7 +29,7 @@ const config = defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
         ['meta', { property: 'og:type', content: 'website' }],
         ['meta', { property: 'og:url', content: 'https://anti-nsfw.netlify.app/' }],
         ['meta', { property: 'og:locale', content: 'en_US' }],
-        ['meta', { property: 'og:image', content: '/meta-image.png' }] 
+        ['meta', { property: 'og:image', content: '/meta-image.png' }]
     ],
     theme: path.join(__dirname, 'theme', 'index.ts'),
     themeConfig: {
@@ -39,6 +39,7 @@ const config = defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
         sidebarDepth: 3,
         editLinks: true,
         smoothScroll: true,
+        backToTop: true,
         lastUpdated: true,
         docsRepo: 'https://github.com/link-discord/anti-nsfw-guide',
         navbar: [
